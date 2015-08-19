22a440a818b5b50003c95fc0c4ed6cc997b8575f
Emily Stamey <elstamey@ncsu.edu>
2015-08-18 20:38:36 -0700

    fixed the B upside down

--- src/Plugin.php
+++ src/Plugin.php
@@ -36,68 +36,68 @@ class Plugin extends AbstractPlugin
     {
         //https://en.wikipedia.org/wiki/Transformation_of_text#Upside-down_text
         $this->array_upside_down = array(
-            'a' => '\u0250',
-            'b' => '\u0071',
-            'c' => '\u0254',
-            'd' => '\u0070',
-            'e' => '\u01dd',
-            'f' => '\u025f',
-            'g' => '\u0183',
-            'h' => '\u0265',
-            'i' => '\u0131',
-            'j' => '\u027e',
-            'k' => '\u029e',
-            'l' => '\uA781',
-            'm' => '\u026f',
-            'n' => '\u0075',
-            'o' => 'o',
-            'p' => 'd',
-            'q' => 'b',
-            'r' => '\u0279',
-            's' => '\u0073',
-            't' => '\u0287',
-            'u' => 'n',
-            'v' => '\u028C',
-            'w' => '\u028D',
-            'x' => '\u0078',
-            'y' => '\u028E',
-            'z' => '\u007A',
-            'A' => '\u2200',
-            'B' => '\u10412',
-            'C' => '\u0186',
-            'D' => '\u15E1',
-            'E' => '\u018E',
-            'F' => '\u2132',
-            'G' => '\u2141',
-            'H' => '\u0048',
-            'I' => '\u0049',
-            'J' => '\u017F',
-            'K' => '\u22CA',
-            'L' => '\u2142',
-            'M' => '\u0057',
-            'N' => '\u004E',
-            'O' => 'O',
-            'P' => '\u0500',
-            'Q' => '\u038C',
-            'R' => '\u1D1A',
-            'S' => '\u0053',
-            'T' => '\u22A5',
-            'U' => '\u2229',
-            'V' => '\u039B',
-            'W' => '\u004D',
-            'X' => '\u0058',
-            'Y' => '\u2144',
-            'Z' => '\u005A',
-            '0' => '0',
-            '1' => '\u0196',
-            '2' => '\u1105',
-            '3' => '\u0190',
-            '4' => '\u3123',
-            '5' => '\u03DB',
-            '6' => '9',
-            '7' => '\u3125',
-            '8' => '8',
-            '9' => '6'
+            'a' => hexdec('0250'),
+            'b' => hexdec('0071'),
+            'c' => hexdec('0254'),
+            'd' => hexdec('0070'),
+            'e' => hexdec('01dd'),
+            'f' => hexdec('025f'),
+            'g' => hexdec('0183'),
+            'h' => hexdec('0265'),
+            'i' => hexdec('0131'),
+            'j' => hexdec('027e'),
+            'k' => hexdec('029e'),
+            'l' => hexdec('A781'),
+            'm' => hexdec('026f'),
+            'n' => hexdec('0075'),
+            'o' => hexdec('006F'),
+            'p' => hexdec('0064'),
+            'q' => hexdec('0062'),
+            'r' => hexdec('0279'),
+            's' => hexdec('0073'),
+            't' => hexdec('0287'),
+            'u' => hexdec('006E'),
+            'v' => hexdec('028C'),
+            'w' => hexdec('028D'),
+            'x' => hexdec('0078'),
+            'y' => hexdec('028E'),
+            'z' => hexdec('007A'),
+            'A' => hexdec('2200'),
+            'B' => hexdec('10412'),
+            'C' => hexdec('0186'),
+            'D' => hexdec('15E1'),
+            'E' => hexdec('018E'),
+            'F' => hexdec('2132'),
+            'G' => hexdec('2141'),
+            'H' => hexdec('0048'),
+            'I' => hexdec('0049'),
+            'J' => hexdec('017F'),
+            'K' => hexdec('22CA'),
+            'L' => hexdec('2142'),
+            'M' => hexdec('0057'),
+            'N' => hexdec('004E'),
+            'O' => hexdec('004F'),
+            'P' => hexdec('0500'),
+            'Q' => hexdec('038C'),
+            'R' => hexdec('1D1A'),
+            'S' => hexdec('0053'),
+            'T' => hexdec('22A5'),
+            'U' => hexdec('2229'),
+            'V' => hexdec('039B'),
+            'W' => hexdec('004D'),
+            'X' => hexdec('0058'),
+            'Y' => hexdec('2144'),
+            'Z' => hexdec('005A'),
+            '0' => hexdec('0030'),
+            '1' => hexdec('0196'),
+            '2' => hexdec('1105'),
+            '3' => hexdec('0190'),
+            '4' => hexdec('3123'),
+            '5' => hexdec('03DB'),
+            '6' => hexdec('0039'),
+            '7' => hexdec('3125'),
+            '8' => hexdec('0038'),
+            '9' => hexdec('0036')
         );
     }
 
@@ -130,7 +130,7 @@ class Plugin extends AbstractPlugin
         foreach ($words as $word) {
             $letters = str_split($word, 1);
             foreach ($letters as $letter) {
-                $new_string = json_decode('"'.$this->array_upside_down[$letter].'"') . $new_string;
+                $new_string = $this->utf8_chr($this->array_upside_down[$letter]) . $new_string;
             }
             $new_string = " " . $new_string;
         }
@@ -140,5 +140,31 @@ class Plugin extends AbstractPlugin
         $queue->ircPrivmsg($channel, $new_string);
     }
 
+    private function utf8_chr($cp) {
 
+        if (!is_int($cp)) {
+            exit("$cp is not integer\n");
+        }
+
+        // UTF-8 prohibits characters between U+D800 and U+DFFF
+        // https://tools.ietf.org/html/rfc3629#section-3
+        //
+        // Q: Are there any 16-bit values that are invalid?
+        // http://unicode.org/faq/utf_bom.html#utf16-7
+
+        if ($cp < 0 || (0xD7FF < $cp && $cp < 0xE000) || 0x10FFFF < $cp) {
+            exit("$cp is out of range\n");
+        }
+
+        if ($cp < 0x10000) {
+            return json_decode('"\u'.bin2hex(pack('n', $cp)).'"');
+        }
+
+        // Q: Isn’t there a simpler way to do this?
+        // http://unicode.org/faq/utf_bom.html#utf16-4
+        $lead = 0xD800 - (0x10000 >> 10) + ($cp >> 10);
+        $trail = 0xDC00 + ($cp & 0x3FF);
+
+        return json_decode('"\u'.bin2hex(pack('n', $lead)).'\u'.bin2hex(pack('n', $trail)).'"');
+    }
 }
